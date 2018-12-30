3e843c12a790b4891b76f8c7cb184276e455e416
angelks <angelks4@live.com>
2018-12-30 20:04:13 +0800

    feat: add styled commponent

--- src/button-demo.js
+++ src/button-demo.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 import AnimationButton from "./react-hover-button";
+import "./styles.scss";
 
 function ButtonDemoList() {
   return (
     <div>
-      <AnimationButton />
+      <AnimationButton
+        hoverBackColor="#0093ff"
+        background={"linear-gradient(to right, #6dd5ed, #2193b0)"}
+      >
+        确定
+      </AnimationButton>
     </div>
   );
 }
--- src/react-hover-button.js
+++ src/react-hover-button.js
@@ -1,10 +1,65 @@
 import React, { Component } from "react";
-import "./styles.scss";
+import styled from "styled-components";
 
-export default function AnimationButton({ color }) {
+export default function AnimationButton({
+  color,
+  background,
+  hoverBackColor,
+  children = "Hover me",
+  onClick
+}) {
+  const Mask = styled.a`
+    --p: 0;
+    --q: calc(1 - var(--p));
+    overflow: hidden;
+    position: relative;
+    display: block;
+    z-index: 1;
+    width: 12em;
+    background: ${({ backgroundColor }) =>
+      backgroundColor ? backgroundColor : "#444"};
+    color: #000;
+    font: 700 1.125em/ 3 trebuchet ms, sans-serif;
+    text-align: center;
+    text-decoration: none;
+    text-transform: uppercase;
+    &:before,
+    &:after {
+      --i: var(--p);
+      --j: calc(1 - var(--i));
+      position: absolute;
+      z-index: -1;
+      top: 0;
+      bottom: 0;
+      left: calc(var(--j) * (100% - 12.36932em));
+      width: 12.36932em;
+      transform-origin: calc(var(--j) * 100%) calc(var(--i) * 100%);
+      transform: rotate(-14.03624deg)
+        translate(calc(var(--q) * (1 - 2 * var(--i)) * -100%));
+      box-shadow: 0 0 0 1px currentcolor;
+      background: currentcolor;
+      color: ${({ hoverBackColor }) =>
+        hoverBackColor ? hoverBackColor : "#ef4654"};
+      transition: transform 0.5s ease-in-out;
+      content: "";
+    }
+    &:after {
+      --i: var(--q);
+    }
+    &:hover,
+    &:focus {
+      --p: 1;
+    }
+  `;
   return (
-    <a className="a" style={{ color: color }}>
-      Click me
-    </a>
+    <>
+      <Mask
+        hoverBackColor={hoverBackColor}
+        onClick={onClick}
+        backgroundColor={background}
+      >
+        {children}
+      </Mask>
+    </>
   );
 }
